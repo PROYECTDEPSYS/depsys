c425f3dff62bd4f4c307c75b877e5b6c1d3aba13
mvs <visomi.4@openmailbox.org>
2016-01-26 13:09:49 -0600

    Se realiza reestructuración debido a fallos en la librería lectora/escritora de configuración

--- app/depsys.sh
+++ app/depsys.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/zsh
 
+#Compilación del archivo
 gcc -o depsys `xml2-config --cflags --libs` `pkg-config gtk+-3.0 --cflags --libs` `mysql_config --cflags --libs` depsys.c
-
+#Ejecución del archivo
 ./depsys
